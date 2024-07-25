9af5bb12b4055af06f9e8e31fe38d3cb628d5ae1
FarAwayJams <159678961+FarAway6834@users.noreply.github.com>
2024-07-25 08:29:31 +0000

    불안해...진짜

--- hfilegen.py
+++ hfilegen.py
@@ -65,7 +65,8 @@ txtloader, txtdumper = wither(o)(
 
 def hfilegen_basic(f):
   fnt, ext = s(f)
-  assert ext == '.myhc', TypeError(f'Only MyHCore file supports, [file : {f}]')
+  assert ext == '.myhc', TypeError(
+      f'Only MyHCore file supports in hfilegen_basic, [file : {f}]')
   fn = f'{fnt}.h'
   fhn = fn.replace('.', '_').upper()
   fhn = f'_{fhn}'
@@ -77,3 +78,111 @@ def hfilegen_basic(f):
 {src}
 
 #endif''')
+
+
+readliner = wither(o)((lambda f: f.readlines()))
+
+
+def formstrer(x, realform='#{} {}', coresrc='define'):
+  return realform.format(coresrc, x)
+
+
+def define_hfilegen_core(f, realform='#{} {}', coresrc='define'):
+  yield from (formstrer(x, realform=realform, coresrc=coresrc)
+              for i in readliner(f))
+
+
+def define_hfilegen_writer(f, realform='#{} {}', coresrc='define'):
+
+  def define_hfilegen_writer_core(fp):
+    for i in define_hfilegen_core(f, realform=realform, coresrc=coresrc):
+      fp.writelines(i)
+
+  return define_hfilegen_writer_core
+
+
+def define_hfilegen(f):
+  fnt, ext = s(f)
+  assert ext == '.domh', TypeError(
+      f'Only Defs of MyH file supports in define_hfilegen, [file : {f}]')
+  fn = f'domh{fnt}.myhc'
+  wither(w)(define_hfilegen_writer(f))(fn)
+
+
+def includes_hfilegen(f):
+  fnt, ext = s(f)
+  assert ext == '.iomh', TypeError(
+      f'Only Includes of MyH file supports in includes_hfilegen, [file : {f}]')
+  fn = f'includer{fnt}.myhc'
+  wither(w)(define_hfilegen_writer(f, coresrc='include'))(fn)
+
+
+def std_includes_hfilegen(f):
+  fnt, ext = s(f)
+  assert ext == '.siomh', TypeError(
+      f'Only std ionh file supports in std_includes_hfilegen, [file : {f}]')
+  fn = f'std{fnt}.iomh'
+  wither(w)(define_hfilegen_writer(f, formstrer='{}<{}>', coresrc=''))(fn)
+
+
+def unstd_includes_hfilegen(f):
+  fnt, ext = s(f)
+  assert ext == '.xiomh', TypeError(
+      f'Only unstd ionh file supports in unstd_includs_hfilegen, [file : {f}]')
+  fn = f'unstd{fnt}.iomh'
+  wither(w)(define_hfilegen_writer(f, formstrer='{}"{}"', coresrc=''))(fn)
+
+
+def typeheaders_gen(f):
+  fnt, ext = s(f)
+  assert ext == '.thmh', TypeError(
+      f'Only TypeHeader MyH file supports in typeheaders_gen, [file : {f}]')
+  fn = f'types{fnt}.myhc'
+  wither(w)(define_hfilegen_writer(f, coresrc='typedef'))(fn)
+
+
+def linextering(f):
+  txtdumper(f'f2lned{f}.f2ln')(f)(txtloader(f).replace('\\', '\\\\').replace(
+      'F2LN\n', 'F2LN\\n'))
+
+
+def unlinextering(f):
+  fnt, ext = s(f)
+  assert ext == '.f2ln', TypeError(
+      f"can't unlinextering, not linexter fike. [file : {f}]")
+  fn = f'types{fnt}.myhc'
+  txtdumper(f'{fn}')(f)(txtloader(f).replace('F2LN\\n',
+                                             'F2LN\n').replace('\\\\', '\\'))
+
+
+class NtypecheckMyhdirNchanges:
+
+  class jtft:
+
+    @staticmethod
+    def xing(x, y):
+      pass
+
+    @staticmethod
+    def unxing(x, y):
+      pass
+
+  class x2y:
+
+    @staticmethod
+    def dirs(x, y):
+      pass
+
+    @staticmethod
+    def jtft(x, y):
+      pass
+
+  class y2x:
+
+    @staticmethod
+    def dirs(x, y):
+      pass
+
+    @staticmethod
+    def jtft(x, y):
+      pass
